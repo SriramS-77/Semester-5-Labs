b455736d91e04c8b40610c81149374c3128e7e50
SriramS-77 <123828917+SriramS-77@users.noreply.github.com>
2024-08-03 16:13:19 +0530

    Add files via upload

---
+++ CV-Lab/Lab-1/q1.py
@@ -0,0 +1,12 @@
+import cv2 as cv
+
+img = cv.imread("./Res/dog.jpeg")
+
+img = cv.resize(img, dsize=(img.shape[1]//5, img.shape[0]//5))
+
+cv.imshow('frame', img)
+cv.waitKey(0)
+
+cv.destroyAllWindows()
+
+cv.imwrite("./Res/duplicate_dog.jpeg", img)
---
+++ CV-Lab/Lab-1/q6.py
@@ -0,0 +1,16 @@
+import imutils
+import cv2 as cv
+
+img = cv.imread("./Res/dog.jpeg")
+
+img = cv.resize(img, dsize=(img.shape[1]//5, img.shape[0]//5))
+
+cv.imshow('Resized frame', img)
+cv.waitKey(0)
+
+img = imutils.rotate(img, 315)
+
+cv.imshow('Rotated frame', img)
+cv.waitKey(0)
+
+cv.destroyAllWindows()
