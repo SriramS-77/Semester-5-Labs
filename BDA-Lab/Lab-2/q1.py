f73e468ebced742fc336165e343dd778af1830e3
SriramS-77 <123828917+SriramS-77@users.noreply.github.com>
2024-08-12 11:16:54 +0530

    Delete BDA-Lab/Lab-2 directory

--- BDA-Lab/Lab-2/q1.py
+++
@@ -1,7 +0,0 @@
-from pyspark import SparkContext
-
-sc = SparkContext("local", "testing")
-
-lst = list(map(int, input("Enter space separated list of integers: ").split()))
-
-print(sc.parallelize(lst).map(lambda x: x**2).collect())
